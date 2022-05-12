beb9b0f1412bff4ad217f217731a3ec709c3539b
Maxim Kazakov <m.c.kazakov.developer@gmail.com>
2022-05-12 15:25:22 +0300

    Initial commit

---
+++ src/main/java/com/example/devopspracticesandtools/DevOpsPracticesAndToolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopspracticesandtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsPracticesAndToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevOpsPracticesAndToolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devopspracticesandtools/DevOpsPracticesAndToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopspracticesandtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsPracticesAndToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
